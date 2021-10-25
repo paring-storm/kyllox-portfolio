120454bb23481f7bc78d072b3afd4202bfdc38ba
pikokr <parangee9706@gmail.com>
2021-10-25 18:23:26 +0900

    mobile menu button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Profile from '@assets/profile.svg'
+import Menu from '@assets/Menu.svg'
 
 const Header: React.FC = () => {
   return (
@@ -10,18 +11,23 @@ const Header: React.FC = () => {
       <div className="container flex items-center gap-8">
         <img src={Profile.src} className="w-16 h-16" alt="" />
         <div className="flex-grow" />
-        <a className="text-3xl font-light font-spoqa-han-sans" href="#home">
-          Home
-        </a>
-        <a className="text-3xl font-light font-spoqa-han-sans" href="#skills">
-          Skills
-        </a>
-        <a
-          className="text-3xl font-light font-spoqa-han-sans"
-          href="#connection"
-        >
-          Connection
-        </a>
+        <div className="gap-8 hidden md:flex">
+          <a className="text-3xl font-light font-spoqa-han-sans" href="#home">
+            Home
+          </a>
+          <a className="text-3xl font-light font-spoqa-han-sans" href="#skills">
+            Skills
+          </a>
+          <a
+            className="text-3xl font-light font-spoqa-han-sans"
+            href="#connection"
+          >
+            Connection
+          </a>
+        </div>
+        <div className="block md:hidden">
+          <img src={Menu.src} className="w-16 h-16 cursor-pointer" alt="" />
+        </div>
       </div>
     </div>
   )
