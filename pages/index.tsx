d1bc899deee1bd1c53618ed22b48a1ec1cdb999e
pikokr <parangee9706@gmail.com>
2021-10-25 16:06:43 +0900

    install fullpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import { NextPage } from 'next'
+import ReactFullpage from '@fullpage/react-fullpage'
 
 const Home: NextPage = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      <ReactFullpage
+        render={() => (
+          <div>
+            <div className="section">1</div>
+            <div className="section">2</div>
+            <div className="section">3</div>
+          </div>
+        )}
+      />
+    </div>
+  )
 }
 
 export default Home
